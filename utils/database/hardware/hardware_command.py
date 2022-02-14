56a959a6a2d53342f6ddcae18f0214d568015057
Siratee K <sirateek@gmail.com>
2022-02-15 00:55:29 +0700

    Add document for the get_command method

--- utils/database/hardware/hardware_command.py
+++ utils/database/hardware/hardware_command.py
@@ -12,10 +12,20 @@ class HardwareCommand(DatabaseBase):
         self.__collection: collection = self.db[hardware_collection_name]
 
     def get_command(self, hardwareId: str) -> List[HardwareCommandModel]:
+        """Get all commands in the database based on the inputted
+        hardwareId
+
+        Args:
+            hardwareId (str): The hardwareId to search.
+
+        Returns:
+            List[HardwareCommandModel]: List of the command that associalted with the inputted hardwareId
+        """
         result = self.__collection.find(
             {"hardwareId": hardwareId}, {"_id": 0})
         list_result = list(result)
         command_list = []
+        # Convert everything to into the ResponseModel
         for item in list_result:
             hardware_model = HardwareCommandModel.from_dict(item)
             command_list.append(hardware_model)
