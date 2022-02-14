46a99298f7f34d2214bceec83fc1cdf1ee6dca6f
rattatham <alimokung@gmail.com>
2022-02-15 01:13:32 +0700

    added feature delete remote

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
-from pydantic import BaseModel
 
 # Main App
 app = FastAPI()
@@ -18,18 +17,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-class Remote(BaseModel):
-  remoteId: int
-  remoteName: str
-  structure: str
 
 # Init Database Connection
 client = MongoClient('mongodb://localhost', 27017)
 db = client["SmartRemote"]
+remote_collection = db["Remote"]
+
 
 # Main APIs
-@app.post("/{{api_host}}/remote/:remoteId/generate/")
-def main_api_age():
+@app.delete("/{{api_host}}/remote/{remote_id}}/")
+def delete_remote(remote_id: int):
+    query_remoteID = {
+      "remoteID": remote_id
+      }
+    query_result = remote_collection.find(query_remoteID, {"_id": 0})
+    remote_list = list(query_result)
+    if len(list_query_toilet) != 1:
+        raise HTTPException(400, {
+            "message": "The remote id was not found or there were 2 or more remotes found."
+        })
+    remote_collection.delete_one(query_remoteID)
     return {
-        "message": "success"
+        "message": f"Remote {remote_id} deleted"
     }
